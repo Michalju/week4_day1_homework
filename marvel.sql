5c89bf3a51d199ee23d215cc98c5b4cfd98d4b35
Michalju <juszkin@gmail.com>
2020-11-23 17:13:43 +0000

    Done Q4 and Q5

--- marvel.sql
+++ marvel.sql
@@ -58,9 +58,17 @@ SELECT name from people;
 -- Q3: Oops! Someone spelled Krusty The Clown's name wrong! Change it to reflect the proper spelling (Crusty should be Krusty).
 UPDATE people SET name = 'Krusty the Clown' WHERE name = 'Crusty the Clown';
 SELECT * from people;
--- Return ONLY Homer Simpson's name from the 'people' table.
--- The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
--- We forgot one of the main characters! Add Bart Simpson to the 'people' table
--- Eric Cartman has decided to hijack our movie evening, Remove him from the table of people.
--- The cinema has just heard that they will be holding an exclusive midnight showing of 'Avengers: Infinity War'!! Create a new entry in the 'movies' table to reflect this.
--- The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and set the show time of "Iron Man 3" to start two hours later.
+-- Q4: Return ONLY Homer Simpson's name from the 'people' table.
+SELECT name from people WHERE id=1;
+SELECT name from people WHERE name = 'Homer Simpson';
+SELECT name from people WHERE name LIKE 'Homer%';
+-- Q5: The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
+DELETE from movies WHERE title = 'Batman Begins';
+SELECT * from movies;
+-- Q6:We forgot one of the main characters! Add Bart Simpson to the 'people' table
+
+-- Q7:Eric Cartman has decided to hijack our movie evening, Remove him from the table of people.
+
+-- Q8:The cinema has just heard that they will be holding an exclusive midnight showing of 'Avengers: Infinity War'!! Create a new entry in the 'movies' table to reflect this.
+
+-- Q9:The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and set the show time of "Iron Man 3" to start two hours later.
